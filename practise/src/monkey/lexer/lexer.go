9510d7c67f0f1d38a9c26eacfe2028fbe4930183
firaga <zhou.jichen@gmail.com>
2022-10-17 10:52:38 +0800

    1.3 简单词法分析

--- practise/src/monkey/lexer/lexer.go
+++ practise/src/monkey/lexer/lexer.go
@@ -1,1 +1,57 @@
 package lexer
+
+import "monkey/token"
+
+type Lexer struct {
+	input        string
+	position     int  //字符串当前位置
+	readPosition int  //字符串读取位置
+	ch           byte //当前位置值
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readchar()
+	return l
+}
+
+func (l *Lexer) readchar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition++
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+	l.readchar()
+	return tok
+}
+
+func newToken(tok token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tok, Literal: string(ch)}
+}
--- practise/src/monkey/token/token.go
+++ practise/src/monkey/token/token.go
@@ -1,1 +1,47 @@
+// token/token.go
+
 package token
+
+//let five = 5;
+//let ten = 10;
+//
+//let add = fn(x, y) {
+//  x + y;
+//};
+//
+//let result = add(five, ten);
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+// token/token.go
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// 标识符+字面量
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+
+	// 运算符
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// 分隔符
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// 关键字
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
